f3a6a4d7117540d6706c28276740db2cdbbf3a73
Jonathan Hayes <jon.hayes37@gmail.com>
2023-12-22 22:32:58 -0500

    Add unit testing

--- iu/commands/calendar.py
+++ iu/commands/calendar.py
@@ -6,7 +6,7 @@ CALENDAR_LINK = "https://calendar.google.com/calendar/ical/b6a7d2e436aed7053f51b
 "05319da47db22396c2909cf94%40group.calendar.google.com/public/basic.ics"
 
 async def send_calendar(interaction):
-    calendar_gif = discord.File('media/gifs/calendar_tutorial.gif',
+    calendar_gif = discord.File('iu/media/gifs/calendar_tutorial.gif',
                                 filename='calendar_tutorial.gif')
     await interaction.response.send_message(
         "Here's the <:hallyu:795848873910206544> calendar with all of our events! " \
