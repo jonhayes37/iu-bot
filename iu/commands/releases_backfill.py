72dc6aa9a500a74c2357dff7f5b02cc960aa1b8d
Jonathan Hayes <jon.hayes37@gmail.com>
2024-11-09 15:29:04 -0500

    Fix 15

--- iu/commands/releases_backfill.py
+++ iu/commands/releases_backfill.py
@@ -1,7 +1,9 @@
 from triggers.message import store_new_release
 
 async def backfill_releases(channel):
-    messages = [message async for message in channel.history(limit=10000)]
+    messages = [message async for message in channel.history(limit=1000)]
     print(f'retrieved {len(messages)} messages')
     for msg in messages:
         store_new_release(msg)
+
+    print('finished parsing backfills!')
